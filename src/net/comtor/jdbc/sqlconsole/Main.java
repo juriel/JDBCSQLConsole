09e13d5c2021b135a01755b830f34e902ae21cfc
Jaime Uriel <juriel@comtor.net>
2019-01-30 18:24:27 -0500

    LOCAL

---
+++ src/net/comtor/jdbc/sqlconsole/Main.java
@@ -0,0 +1,17 @@
+
+package net.comtor.jdbc.sqlconsole;
+
+/**
+ *
+ * @author juriel
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
